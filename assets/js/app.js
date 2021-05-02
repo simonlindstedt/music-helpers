a7fd479211eb93102fe1549b672abae55e139784
simonlindstedt <simon.lindstedt@gmail.com>
2021-05-02 11:33:37 +0200

    cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,2 @@
 import Scale from "./scale-module.js";
-
-// let arr = [
-//   new Scale("C"),
-//   new Scale("C#"),
-//   new Scale("Db"),
-//   new Scale("D"),
-//   new Scale("Eb"),
-//   new Scale("E"),
-//   new Scale("F"),
-//   new Scale("Gb"),
-//   new Scale("G"),
-//   new Scale("Ab"),
-//   new Scale("A"),
-//   new Scale("Bb"),
-//   new Scale("B"),
-// ];
-
-// console.log(arr);
+import EuclidianRythm from "./euclidian-rythm-module.js";
